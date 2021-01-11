d3f45bfb0412332b6075f91003244a587de94c97
Clems-web <clement.servais1@gmail.com>
2021-01-11 16:45:28 +0100

    Reste les évènements à faire

---
+++ app.js
@@ -0,0 +1,31 @@
+$("#container").css( {
+    "width": "50%",
+    "height": "70rem",
+    "margin-left": "25%",
+    "margin-top": "2rem",
+    "border": "1px solid black",
+    "display" : "flex",
+    "justify-content": "space-evenly",
+    "flex-wrap": "wrap",
+    "padding": "0.9rem"
+})
+
+$(".square").css( {
+    "width": "12.5rem",
+    "height" : "12.5rem",
+    "border": "1px solid black"
+})
+
+$(".circle").css( {
+    "width": "12.5rem",
+    "height" : "12.5rem",
+    "border": "1px solid black",
+    "border-radius": "100px"
+})
+
+$(".losange").css( {
+    "width": "12.5rem",
+    "height" : "12.5rem",
+    "border": "1px solid black",
+    "transform": "rotate(45deg)"
+})
