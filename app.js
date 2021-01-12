2d9469295d974c6fe397edf78ae9ba12cc8bf392
Clems-web <clement.servais1@gmail.com>
2021-01-12 11:43:27 +0100

    pause

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $("#container").css( {
     "width": "50%",
-    "height": "70rem",
+    "height": "68rem",
     "margin-left": "25%",
     "margin-top": "2rem",
     "border": "1px solid black",
@@ -8,24 +8,44 @@ $("#container").css( {
     "justify-content": "space-evenly",
     "flex-wrap": "wrap",
     "padding": "0.9rem"
-})
+});
 
 $(".square").css( {
     "width": "12.5rem",
     "height" : "12.5rem",
     "border": "1px solid black"
-})
+}).click(function () {
+    $(this).toggleClass("black");
+});
 
 $(".circle").css( {
     "width": "12.5rem",
     "height" : "12.5rem",
     "border": "1px solid black",
     "border-radius": "100px"
-})
+}).click(function () {
+    $(this).toggleClass("black");
+});
 
 $(".losange").css( {
     "width": "12.5rem",
     "height" : "12.5rem",
     "border": "1px solid black",
     "transform": "rotate(45deg)"
-})
+});
+
+$(".btn").css({
+    "width": "40%",
+    "height": "4rem",
+    "font-size": "2rem",
+    "margin-left": "30%",
+    "margin-top": "2rem"
+});
+
+$("#change").click(function () {
+    $(".circle").toggleClass("black");
+    $(".square").toggleClass("black");
+    $(".losange").toggleClass("black");
+});
+
+
